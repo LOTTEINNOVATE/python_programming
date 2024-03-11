a958e10a4831d96132dda84cca76e8b57990c32a
LOTTEINNOVATE <162481044+LOTTEINNOVATE@users.noreply.github.com>
2024-03-12 00:22:50 +0900

    Add files via upload

---
+++ 2주차 숙제2_정용훈(2024275570).py
@@ -0,0 +1,28 @@
+# Exercise Strings 1
+A = "1"
+print (A)
+
+B = "2"
+print (B)
+
+C = A + B
+print (C)
+
+D = "ABCDEFG"
+print (D[0:3])
+
+E = 'clocrkrlelclt'
+print(E[::2])
+
+print("Jeong Yong Hun \\ 2024275570")
+
+F = "You are wrong"
+print(F.upper())
+
+G = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+print(G.find("snow"))
+print(G.replace("Mary", "Bob"))
+
+# 감사합니다.
