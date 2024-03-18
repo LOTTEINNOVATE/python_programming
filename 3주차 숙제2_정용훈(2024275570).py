3a285818c8111177dd431c5752f80db050177cca
LOTTEINNOVATE <162481044+LOTTEINNOVATE@users.noreply.github.com>
2024-03-18 14:06:37 +0900

    Add files via upload

---
+++ 3주차 숙제2_정용훈(2024275570).py
@@ -0,0 +1,22 @@
+# 3주차 숙제 2 입니다.
+
+print("1번 문제 : Create a list 'a_list', with the following elements '1, hello, [1, 2, 3]' and 'True'")
+a_list = [1, "hello", [1, 2, 3], "True"]
+print("1번 문제 정답 : ", a_list)
+print("=================================================")
+print("2번 문제 : Find the value stored at index 1 of 'a_list'.")
+a_list[1] #교수님 근데 이거는 적지 않아도 되지 않을까요? print에서 출력할 때 적으니까요.
+print("2번 문제 정답 : ", a_list[1])
+print("=================================================")
+print("3번 문제 : Retrieve the elements stored at index 1,2 and 3 of 'a_list'.")
+print("현재 a_list의 elements는 ? ", a_list)
+print("3번 문제 정답 : ", a_list[1:4])
+print("=================================================")
+print("4번 문제 : Concatenate the following lists 'A = [1, 'a'] and B = [2, 1, 'd'] :")
+List_A = [1, "a"]
+List_B = [2, 1, "d"]
+print("새로 추가한 리스트 목록 List_A : ", List_A)
+print("새로 추가한 리스트 목록 List_B : ", List_B)
+print("4번 문제 정답 : ", List_A + List_B)
+print("=================================================")
+# 감사합니다.
