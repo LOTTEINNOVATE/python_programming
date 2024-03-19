43beb27813ba70bdf725f746df1e2bbf821d811f
LOTTEINNOVATE <162481044+LOTTEINNOVATE@users.noreply.github.com>
2024-03-19 12:24:24 +0900

    Add files via upload

---
+++ 3주차 숙제3_정용훈(2024275570).py
@@ -0,0 +1,24 @@
+# 3주차 숙제 3 입니다.
+
+print("1번 문제 : You will need this dictionary for the next two questions ")
+soundtrack_dic = {"The Bodyguard" : "1992", "Saturday Night Fever" : "1977"}
+print("Soundtrack_dic 의 목록은? ", soundtrack_dic)
+print("1번의 (a) 문제 : In the dictionary 'soundtrack_dic' what are the keys?")
+print("1번의 (a) 정답 : ", soundtrack_dic.keys())
+print("1번의 (b) 문제 : In the dictionary 'soundtrack_dic' what are the values?")
+print("1번의 (b) 정답 : ", soundtrack_dic.values())
+print("=====================================================")
+print("2번 문제 : You will need this dictionary for the follow questions:")
+print("The Albums Back in Black, "
+      "The Bodyguard and Thriller have the following music recording sales in millions 50, 50 and 65 respectively :")
+print("2번의 (a) 문제 : Create a dictionary 'album_sales_dict' where the keys are the album name and the sales in millions are the values.")
+album_sales_dict = {"Back in Black" : 50, "The Bodyguard" : 50, "Thriller" : 65 }
+print("2번의 (a) 정답 : ", album_sales_dict)
+print("2번의 (b) 문제 : Use the dictionary to find the total sales of thriller")
+print("2번의 (b) 정답 : ", album_sales_dict["Thriller"])
+print("2번의 (c) 문제 : Find the names of the albums from the dictionary using the method keys:")
+print("2번의 (c) 정답 : ", album_sales_dict.keys())
+print("2번의 (d) 문제 : Find the names of the recording sales from the dictionary using method values:")
+print("2번의 (d) 정답 : ", album_sales_dict.values())
+print("=====================================================")
+# 감사합니다.
